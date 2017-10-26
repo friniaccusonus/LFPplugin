f432ca05530e0429d9b9b554db24dd48dc62c36b
friniaccusonus <frini@accusonus.com>
2017-10-26 13:54:22 +0300

    changes

--- LPFilter/Source/PluginProcessor.cpp
+++ LPFilter/Source/PluginProcessor.cpp
@@ -1,12 +1,12 @@
 /*
-  ==============================================================================
-
-    This file was auto-generated!
-
-    It contains the basic framework code for a JUCE plugin processor.
-
-  ==============================================================================
-*/
+ ==============================================================================
+ 
+ This file was auto-generated!
+ 
+ It contains the basic framework code for a JUCE plugin processor.
+ 
+ ==============================================================================
+ */
 
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
@@ -16,18 +16,18 @@
 //==============================================================================
 LpfilterAudioProcessor::LpfilterAudioProcessor()
 #ifndef JucePlugin_PreferredChannelConfigurations
-     : AudioProcessor (BusesProperties()
-                     #if ! JucePlugin_IsMidiEffect
-                      #if ! JucePlugin_IsSynth
-                       // Sets Input and Output bus to be stereo by default
-                       .withInput  ("Input",  AudioChannelSet::stereo(), true)
-                      #endif
-                       .withOutput ("Output", AudioChannelSet::stereo(), true)
-                     #endif
-                       ),
+: AudioProcessor (BusesProperties()
+#if ! JucePlugin_IsMidiEffect
+#if ! JucePlugin_IsSynth
+                  // Sets Input and Output bus to be stereo by default
+                  .withInput  ("Input",  AudioChannelSet::stereo(), true)
 #endif
-        // Setup lpfJuce
-        lpfJuce(dsp::IIR::Coefficients<float>::makeFirstOrderLowPass(44100.0, defaultFreq))
+                  .withOutput ("Output", AudioChannelSet::stereo(), true)
+#endif
+                  ),
+#endif
+// Setup lpfJuce
+lpfJuce(dsp::IIR::Coefficients<float>::makeFirstOrderLowPass(44100.0, defaultFreq))
 
 {
     //Setup filter with DSPFilters lib
@@ -53,29 +53,29 @@ const String LpfilterAudioProcessor::getName() const
 
 bool LpfilterAudioProcessor::acceptsMidi() const
 {
-   #if JucePlugin_WantsMidiInput
+#if JucePlugin_WantsMidiInput
     return true;
-   #else
+#else
     return false;
-   #endif
+#endif
 }
 
 bool LpfilterAudioProcessor::producesMidi() const
 {
-   #if JucePlugin_ProducesMidiOutput
+#if JucePlugin_ProducesMidiOutput
     return true;
-   #else
+#else
     return false;
-   #endif
+#endif
 }
 
 bool LpfilterAudioProcessor::isMidiEffect() const
 {
-   #if JucePlugin_IsMidiEffect
+#if JucePlugin_IsMidiEffect
     return true;
-   #else
+#else
     return false;
-   #endif
+#endif
 }
 
 double LpfilterAudioProcessor::getTailLengthSeconds() const
@@ -86,7 +86,7 @@ double LpfilterAudioProcessor::getTailLengthSeconds() const
 int LpfilterAudioProcessor::getNumPrograms()
 {
     return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
-                // so this should be at least 1, even if you're not really implementing programs.
+    // so this should be at least 1, even if you're not really implementing programs.
 }
 
 int LpfilterAudioProcessor::getCurrentProgram()
@@ -118,21 +118,21 @@ void LpfilterAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlo
     lpfJuce.prepare(spec);
     
     // Prepare lpfDspLib filter
-    paramsDsp[0] = sampleRate; paramsDsp[1] = 1; paramsDsp[2] = defaultFreq;
+    paramsDsp[0] = sampleRate;      // sample rate
+    paramsDsp[1] = 1;               // order
+    paramsDsp[2] = defaultFreq;     // cut-off frequency
     lpfDspLib->setParams(paramsDsp);
     
     // Set up custom LPF coefficients
     iirCoef = IIRCoefficients::makeLowPass(sampleRate, *frequency);
     
-    if (getTotalNumInputChannels() < 2 )
-    {
-        tempBuffer.setSize(2, samplesPerBlock);
-    }
-    
     // Set up previous buffer for custom filter
     prevBuffer.setSize(getTotalNumInputChannels(), samplesPerBlock);
     prevBuffer.clear();
+    
+    filteredBuffer.setSize(2, samplesPerBlock);
 }
+
 
 void LpfilterAudioProcessor::releaseResources()
 {
@@ -164,7 +164,7 @@ void LpfilterAudioProcessor::processBlock (AudioSampleBuffer& ioBuffer, MidiBuff
     ScopedNoDenormals noDenormals;
     const int totalNumInputChannels  = getTotalNumInputChannels();
     const int totalNumOutputChannels = getTotalNumOutputChannels();
-
+    
     // In case we have more outputs than inputs, this code clears any output
     // channels that didn't contain input data, (because these aren't
     // guaranteed to be empty - they may contain garbage).
@@ -178,8 +178,21 @@ void LpfilterAudioProcessor::processBlock (AudioSampleBuffer& ioBuffer, MidiBuff
     //Update frequency parameter
     updateParameters();
     
-    // The filtering process happens here
-    process (ioBuffer);
+    if (mode->getIndex() == 0)
+    {
+        // Filtering with Juce Modules
+        juceModulesProcess (ioBuffer);
+    }
+    else if (mode->getIndex() == 1)
+    {
+        // Filtering with DSPFilters
+        dspFiltersProcess (ioBuffer);
+    }
+    else
+    {
+        // Filtering with custom filter
+        customProcess(ioBuffer);
+    }
     
     // Apply gain
     for (int channel = 0; channel < totalNumInputChannels; ++channel)
@@ -188,68 +201,75 @@ void LpfilterAudioProcessor::processBlock (AudioSampleBuffer& ioBuffer, MidiBuff
     }
 }
 
-void LpfilterAudioProcessor::process (AudioSampleBuffer& processBuffer) noexcept
+void LpfilterAudioProcessor::juceModulesProcess(AudioSampleBuffer& processBuffer) noexcept
 {
-    // Define the block that passes into process function
+    // Define the block that passes into juceModulesProcess function
     dsp::AudioBlock<float> block (processBuffer);
     
-    if (mode->getIndex() == 0)
-    {
-        // lpfJuce filter processing
-        lpfJuce.process(dsp::ProcessContextReplacing<float> (block));
-    }
-    else if (mode->getIndex() == 1)
-    {
-        // lpfDspLib filter processing
+    // lpfJuce filter processing
+    lpfJuce.process(dsp::ProcessContextReplacing<float> (block));
+}
+void LpfilterAudioProcessor::dspFiltersProcess (AudioSampleBuffer& processBuffer) noexcept
+{
+    int bufferChans = processBuffer.getNumChannels(); // channels of the input buffer (1 or 2)
+    int filterChans = filteredBuffer.getNumChannels(); // channels of the filtered buffer (2)
     
-        if (getTotalNumInputChannels() < 2 )
-        {
-            tempBuffer.copyFrom(0, *tempBuffer.getReadPointer(0), processBuffer.getReadPointer(0), processBuffer.getNumSamples());
-            tempBuffer.copyFrom(1, *tempBuffer.getReadPointer(1), processBuffer.getReadPointer(0), processBuffer.getNumSamples());
-        
-            lpfDspLib->process(tempBuffer.getNumSamples(), tempBuffer.getArrayOfWritePointers());
-            
-            processBuffer.copyFrom(0, *processBuffer.getWritePointer(0), tempBuffer.getWritePointer(0), tempBuffer.getNumSamples());
-            
-        }
-        
-        lpfDspLib->process(processBuffer.getNumSamples(), processBuffer.getArrayOfWritePointers());
-    }
-    else
+    // fill filteredBuffer with data from processBuffer
+    for (int iChan = 0; iChan < filterChans; ++iChan)
     {
-        // Custom filter processing
-        for (int ch = 0; ch < getTotalNumInputChannels(); ++ch)
-        {
-        
-            float* const writePtr = processBuffer.getWritePointer(ch);
-            float* const prevWritePtr = prevBuffer.getWritePointer(ch);
-            const float* prevReadPtr = prevBuffer.getReadPointer(ch);
-            const float* readPtr = processBuffer.getReadPointer(ch);
-            
-            writePtr[0] = -iirCoef.coefficients[3] * prevWritePtr[511] -
-                                iirCoef.coefficients[4] * prevWritePtr[510] +
-                                iirCoef.coefficients[0] * readPtr[0] +
-                                iirCoef.coefficients[1] * prevReadPtr[511] +
-                                iirCoef.coefficients[2] * prevReadPtr[510] ;
-            
-            
-            writePtr[1] = -iirCoef.coefficients[3] * writePtr[0] -
-                                iirCoef.coefficients[4] * prevWritePtr[511] +
-                                iirCoef.coefficients[0] * readPtr[1] +
-                                iirCoef.coefficients[1] * readPtr[0] +
-                                iirCoef.coefficients[2] * prevReadPtr[511];
-            for (int sample = 2; sample < processBuffer.getNumSamples(); ++sample)
-            {
-                writePtr[sample]  = -iirCoef.coefficients[3] * writePtr[sample-1] -
-                                    iirCoef.coefficients[4] * writePtr[sample-2] +
-                                    iirCoef.coefficients[0] * readPtr[sample] +
-                                    iirCoef.coefficients[1] * readPtr[sample-1] +
-                                    iirCoef.coefficients[2] * readPtr[sample-2] ;
-            }
-        }
-    
-        prevBuffer = processBuffer;
+        /* left channel of tempBuffer = left channel of processBuffer
+         right channel of tempBuffer = left channel of processBuffer(mono version)
+         or right channel of processBuffer(stereo version)
+         */
+        filteredBuffer.copyFrom(iChan, 0, processBuffer, jmin(iChan, bufferChans - 1), 0, processBuffer.getNumSamples());
     }
+    
+    // process data
+    lpfDspLib->process(filteredBuffer.getNumSamples(), filteredBuffer.getArrayOfWritePointers());
+    
+    for (int iChan = 0; iChan < bufferChans; ++iChan)
+    {
+        // copy data to the output buffer
+        processBuffer.copyFrom(iChan, 0, filteredBuffer, iChan, 0, processBuffer.getNumSamples());
+    }
+    
+}
+
+void LpfilterAudioProcessor::customProcess(AudioSampleBuffer& processBuffer) noexcept
+{
+    
+    for (int ch = 0; ch < getTotalNumInputChannels(); ++ch)
+    {
+        float* const writePtr = processBuffer.getWritePointer(ch);
+        float* const prevWritePtr = prevBuffer.getWritePointer(ch);
+        const float* prevReadPtr = prevBuffer.getReadPointer(ch);
+        const float* readPtr = processBuffer.getReadPointer(ch);
+        int lastSample = processBuffer.getNumSamples() - 1;
+        
+        writePtr[0] = -iirCoef.coefficients[3] * prevWritePtr[lastSample] -
+        iirCoef.coefficients[4] * prevWritePtr[lastSample-1] +
+        iirCoef.coefficients[0] * readPtr[0] +
+        iirCoef.coefficients[1] * prevReadPtr[lastSample] +
+        iirCoef.coefficients[2] * prevReadPtr[lastSample-1] ;
+        
+        
+        writePtr[1] = -iirCoef.coefficients[3] * writePtr[0] -
+        iirCoef.coefficients[4] * prevWritePtr[lastSample] +
+        iirCoef.coefficients[0] * readPtr[1] +
+        iirCoef.coefficients[1] * readPtr[0] +
+        iirCoef.coefficients[2] * prevReadPtr[lastSample];
+        
+        for (int sample = 2; sample < processBuffer.getNumSamples(); ++sample)
+        {
+            writePtr[sample]  = -iirCoef.coefficients[3] * writePtr[sample-1] -
+            iirCoef.coefficients[4] * writePtr[sample-2] +
+            iirCoef.coefficients[0] * readPtr[sample] +
+            iirCoef.coefficients[1] * readPtr[sample-1] +
+            iirCoef.coefficients[2] * readPtr[sample-2] ;
+        }
+    }
+    
+    prevBuffer = processBuffer;
 }
 
 void LpfilterAudioProcessor::updateParameters()
@@ -262,7 +282,7 @@ void LpfilterAudioProcessor::updateParameters()
     lpfDspLib->setParams(paramsDsp);
     
     // Update custom filter coeffiecients
-     iirCoef = IIRCoefficients::makeLowPass(getSampleRate(), *frequency);
+    iirCoef = IIRCoefficients::makeLowPass(getSampleRate(), *frequency);
 }
 
 //==============================================================================
@@ -275,6 +295,7 @@ AudioProcessorEditor* LpfilterAudioProcessor::createEditor()
 {
     return new LpfilterAudioProcessorEditor (*this);
 }
+
 
 //==============================================================================
 void LpfilterAudioProcessor::getStateInformation (MemoryBlock& destData)
@@ -328,3 +349,4 @@ AudioProcessor* JUCE_CALLTYPE createPluginFilter()
 {
     return new LpfilterAudioProcessor();
 }
+
